a2a2b4ce1a52381e4c402553b49315266545863b
stal111 <stal111@gmx.de>
2022-08-22 19:30:23 +0200

    change how data gets stored

--- supporters.py
+++ supporters.py
@@ -3,73 +3,86 @@ import requests
 import json
 
 from tkinter import ttk
-from tkinter.messagebox import showinfo
+
+# load data
+patreon_levels = {"Silver Blacksmith": "silver_blacksmith",
+                  "Golden Fighter": "golden_fighter",
+                  "Arcane Sorcerer": "arcane_sorcerer",
+                  "Nebula Club": "nebula_club"}
+data = {}
+
+
+def load_data():
+    with open("supporters.json") as file:
+        data.update(json.load(file))
+
+
+def write_data():
+    with open("supporters.json", "w") as file:
+        jsonObject = json.dumps(data, indent=4)
+
+        file.write(jsonObject)
+
+
+def get_uuid(name):
+    request = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{name}").json()
+
+    return request["id"]
+
+
+def remove_player(name, write=True):
+    uuid = get_uuid(name)
+
+    for level in patreon_levels.values():
+        levelData = data.get(level)
+
+        if {"id": uuid} in levelData:
+            levelData.remove({"id": uuid})
+
+            if write:
+                write_data()
+
+            return
+
+
+def add_player(name, level):
+    uuid = get_uuid(name)
+
+    remove_player(name, write=False)
+
+    if level not in patreon_levels.keys():
+        return
+
+    data.get(patreon_levels.get(level)).append({"id": uuid})
+
+    write_data()
+
+
+load_data()
 
 # root window
 root = tk.Tk()
-root.geometry("300x150")
+root.geometry("300x180")
 root.resizable(False, False)
 root.title('Mandala Supporters')
 
 # store username and patreon level
-username = tk.StringVar()
-patreonLevel = tk.StringVar()
-
-
-class Popup:
-    def __init__(self, master=None, title: str = "Popup", message: str = ""):
-        if master is None:
-            # If the caller didn't give us a master, use the default one instead
-            master = tk._get_default_root()
-
-        # Create a toplevel widget
-        self.root = tk.Toplevel(master)
-        # A min size so the window doesn't start to look too bad
-        self.root.minsize(200, 60)
-        # Stop the user from resizing the window
-        self.root.resizable(False, False)
-        # If the user presses the `X` in the titlebar of the window call
-        # self.destroy()
-        self.root.protocol("WM_DELETE_WINDOW", self.destroy)
-        # Set the title of the popup window
-        self.root.title(title)
-
-        # Calculate the needed width/height
-        width = max(map(len, message.split("\n")))
-        height = message.count("\n") + 1
-        # Create the text widget
-        self.text = tk.Text(self.root, bg="#f0f0ed", height=height,
-                            width=width, highlightthickness=0, bd=0)
-        # Add the text to the widget
-        self.text.insert("end", message)
-        # Make sure the user can't edit the message
-        self.text.config(state="disabled")
-        self.text.pack()
-
-        # Make sure the user isn't able to spawn new popups while this is
-        # still alive
-        self.root.grab_set()
-        # Stop code execution in the function that called us
-        self.root.mainloop()
-
-    def destroy(self) -> None:
-        # Stop the `.mainloop()` that's inside this class
-        self.root.quit()
-        # Destroy the window
-        self.root.destroy()
+local_username = tk.StringVar()
+local_patreon_level = tk.StringVar()
 
 
 def add_clicked():
-    """ callback when the add button clicked
+    """ callback when the add button is clicked
     """
 
-    request = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{username.get()}").json()
+    add_player(local_username.get(), local_patreon_level.get())
 
-    value = {"id": request["id"], "level": patreonLevel.get()}
 
-    msg = f'You entered email: {username.get()} and password: {patreonLevel.get()}'
+def remove_clicked():
+    """ callback when the remove button is clicked
+    """
 
-    Popup(master=root, title="Json code", message="#" + username.get() + "\n" + json.dumps(value))
+    remove_player(local_username.get())
 
 
 # frame
@@ -80,7 +93,7 @@ frame.pack(padx=10, pady=10, fill='x', expand=True)
 usernameLabel = ttk.Label(frame, text="Username:")
 usernameLabel.pack(fill='x', expand=True)
 
-usernameEntry = ttk.Entry(frame, textvariable=username, )
+usernameEntry = ttk.Entry(frame, textvariable=local_username, )
 usernameEntry.pack(fill='x', expand=True)
 usernameEntry.focus()
 
@@ -89,26 +102,31 @@ patreonLevelLabel = ttk.Label(frame, text="Patreon Level:")
 patreonLevelLabel.pack(fill='x', expand=True)
 
 patreonLevelCombobox = ttk.Combobox(frame,
-                                    textvariable=patreonLevel,
+                                    textvariable=local_patreon_level,
                                     state="readonly",
-                                    values=("Silver Blacksmith", "Golden Fighter", "Arcane Sorcerer", "Nebula Club"))
+                                    values=list(patreon_levels.keys()))
 
 patreonLevelCombobox.pack(fill='x', expand=True)
-patreonLevelCombobox.set("Silver Blacksmith")
+patreonLevelCombobox.set(list(patreon_levels.keys())[0])
 
 # add supporter button
 addButton = ttk.Button(frame, text="Add Supporter", command=add_clicked, state="disabled")
 addButton.pack(fill='x', expand=True, pady=10)
 
+# remove supporter Button
+removeButton = ttk.Button(frame, text="Remove Supporter", command=remove_clicked, state="disabled")
+removeButton.pack(fill='x', expand=True)
+
 
 def username_change_callback(*args):
-    if not username.get():
+    if not local_username.get():
         addButton["state"] = "disabled"
+        removeButton["state"] = "disabled"
     else:
         addButton["state"] = "normal"
+        removeButton["state"] = "normal"
 
 
-username.trace("w", callback=username_change_callback)
+local_username.trace("w", callback=username_change_callback)
 
 root.mainloop()
-
